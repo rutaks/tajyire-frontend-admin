42019fc991854062a6dca3632db2af66ed45a544
rutaks <rootsum.dev@gmail.com>
2020-07-19 23:17:43 +0200

    Setup Get Category Details & Sub categories creation redux

---
+++ src/redux/reducers/category/getAllCategoriesReducer.js
@@ -0,0 +1,29 @@
+import { actionTypes } from '../../action-types';
+
+export default (state, { type, payload }) => {
+  switch (type) {
+    case actionTypes.category.GET_ALL_CATEGORIES_START:
+      return {
+        ...state,
+        categoryPayload: {},
+        getAllCategories: { success: false, message: null, loading: true, error: null }
+      };
+    case actionTypes.category.GET_ALL_CATEGORIES_END:
+      return {
+        ...state,
+        getAllCategories: { ...state.getAllCategories, loading: false }
+      };
+    case actionTypes.category.GET_ALL_CATEGORIES_SUCCESS:
+      return {
+        ...state,
+        getAllCategories: { ...state.getAllCategories, payload: payload.payload, success: true }
+      };
+    case actionTypes.category.GET_ALL_CATEGORIES_FAILURE:
+      return {
+        ...state,
+        getAllCategories: { ...state.getAllCategories, error: payload.message }
+      };
+    default:
+      return null;
+  }
+};
---
+++ src/redux/reducers/category/getSubCategoriesReducer.js
@@ -0,0 +1,28 @@
+import { actionTypes } from '../../action-types';
+
+export default (state, { type, payload }) => {
+  switch (type) {
+    case actionTypes.category.GET_SUBCATEGORIES_START:
+      return {
+        ...state,
+        getSubCategories: { payload: {}, success: false, message: null, loading: true, error: null }
+      };
+    case actionTypes.category.GET_SUBCATEGORIES_END:
+      return {
+        ...state,
+        getSubCategories: { ...state.getSubCategories, loading: false }
+      };
+    case actionTypes.category.GET_SUBCATEGORIES_SUCCESS:
+      return {
+        ...state,
+        getSubCategories: { ...state.getSubCategories, payload: payload.payload, success: true }
+      };
+    case actionTypes.category.GET_SUBCATEGORIES_FAILURE:
+      return {
+        ...state,
+        getSubCategories: { ...state.getSubCategories, error: payload.message }
+      };
+    default:
+      return null;
+  }
+};
