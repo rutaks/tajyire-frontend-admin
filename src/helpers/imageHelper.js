018e552e3dc74e1685a34b72ff47478bab12f5ee
rutaks <rootsum.dev@gmail.com>
2020-07-25 15:26:39 +0200

    Added Yup custom validations, Add async Axios helper

--- src/helpers/imageHelper.js
+++ src/helpers/imageHelper.js
@@ -3,3 +3,11 @@ export function getBase64(img, callback) {
   reader.addEventListener('load', () => callback(reader.result));
   reader.readAsDataURL(img);
 }
+export function getBase64Async(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = (error) => reject(error);
+  });
+}
